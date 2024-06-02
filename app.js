102bfab234bbb6c73c33d9594a5132ff6fa53107
abdurakhmonovsharif <fullstackdeveloper4413@gmail.com>
2024-06-02 10:01:23 +0500

    update

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const { start } = require("./connectDB/db");
-start(app);
+// const { start } = require("./connectDB/db");
+// start(app);
 
-const { routers } = require("./routers/routers");
-const path = require("path");
-const cron = require("node-cron");
-const { sendMessage } = require("./routers/message/message");
-routers(app);
+// const { routers } = require("./routers/routers");
+// const path = require("path");
+// const cron = require("node-cron");
+// const { sendMessage } = require("./routers/message/message");
+// routers(app);
 
-sendMessage();
-setInterval(() => {
-  sendMessage();
-}, 1000 * 60 * 60 * 12);
+// sendMessage();
+// setInterval(() => {
+//   sendMessage();
+// }, 1000 * 60 * 60 * 12);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "./frontend", "build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static(path.join(__dirname, "./frontend", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./frontend", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./frontend", "build", "index.html"));
+//   });
+// }
